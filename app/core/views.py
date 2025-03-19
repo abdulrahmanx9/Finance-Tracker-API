076942af309e59eaf015affe5af971bec0ea0728
abdulrahman <ug_31107079@ics.tanta.edu.eg>
2025-03-19 16:30:35 +0200

    setup the project

---
+++ app/core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+import requests
+
+# url = 'https://api.exchangerate-api.com/v4/latest/usd'
+# response = requests.get(url)
+# data = response.json()
+
+# print(data)
